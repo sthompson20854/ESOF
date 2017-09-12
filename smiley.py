83562cbe344289caf7b4e0d149936553910953ae
Steven Thompson <steventhompson@Stevens-MacBook-Pro.local>
2017-09-12 12:26:20 -0600

    smile

---
+++ smiley.py
@@ -0,0 +1,85 @@
+##Ex 15: Write and test a function to meet this specification.
+##drawFace (center, size, win) center is a Point, size is an int, and
+##win is a GraphWin. Draws a simple face of the given size in win.
+##Your function can draw a simple smiley (or grim) face. Demonstrate the
+##function by writing a program that draws several faces of varying size in a
+##single window
+ 
+from graphics import *
+ 
+win = GraphWin('Smiley Faces', 400, 400) # give title and dimensions
+win.setBackground('cyan')
+win.setCoords(0, 0, 400, 400)
+ 
+def drawFace(center, size,window):
+ 
+    head = Circle(center,size*20)
+    head.setFill("green")
+    head.draw(win)
+ 
+    mouth = Circle(center, size*13)
+    mouth.setFill("red")
+    mouth.setOutline("red")
+    mouth.draw(win)
+    smile = Circle(center, size*14)
+    smile.move(0,size*4)
+    smile.setFill("green")
+    smile.setOutline("green")
+    smile.draw(win)
+ 
+    eyebrow = Circle(center,size*4)
+    eyebrow.move(-size*8,size*10)
+    eyebrow.setFill('black')
+    eyebrow.draw(win)
+    eyebrow2 = eyebrow.clone()
+    eyebrow2.draw(win)
+    eyebrow2.move(size*16, 0)
+    eyecircle = Circle(center,size*4)
+    eyecircle.move(-size*8,size*9)
+    eyecircle.setFill('green')
+    eyecircle.setOutline('green')
+    eyecircle.draw(win)
+    eyecircle2 = eyecircle.clone()
+    eyecircle2.draw(win)
+    eyecircle2.move(size*16, 0)
+ 
+    eyelid = Circle(center,size*3)
+    eyelid.move(-size*8,size*8)
+    eyelid.setFill('brown')
+    eyelid.draw(win)
+    eyelid2 = eyelid.clone()
+    eyelid2.draw(win)
+    eyelid2.move(size*16, 0)
+ 
+    eye = Circle(center, size*3)
+    eye.move(-size*8,size*6)
+    eye.setFill('orange')
+    eye.draw(win)
+    eye2 = eye.clone()
+    eye2.draw(win)
+    eye2.move(size*16, 0)
+ 
+    pupil = Circle(center,size)
+    pupil.move(-size*9,size*7)
+    pupil.setFill('blue')
+    pupil.draw(win)
+    pupil2 = pupil.clone()
+    pupil2.draw(win)
+    pupil2.move(size*16,0)
+ 
+    nose = Circle(center,size*3)
+    nose.move(0,-size*2)
+    nose.setOutline('yellow')
+    nose.setFill('yellow')
+    nose.draw(win)
+ 
+def main():
+ 
+ 
+    message = Text(Point(200, 380), 'Click anywhere to quit.')
+    message.setFill('blue')
+    message.draw(win)
+    win.getMouse()
+    win.close()
+ 
+main()
